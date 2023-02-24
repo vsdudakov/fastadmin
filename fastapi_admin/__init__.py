791147870352be6256fbf0c9457f07d44e353078
Seva D <vsdudakov@gmail.com>
2023-02-24 10:50:30 +0400

    Preparing for pip package

---
+++ fastapi_admin/__init__.py
@@ -0,0 +1,3 @@
+from .decorators import register
+from .main import admin_app
+from .models import ModelAdmin, TortoiseModelAdmin, register_admin_model
---
+++ fastapi_admin/settings.py
@@ -0,0 +1,34 @@
+import os
+
+from environs import Env
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+env = Env()
+
+ENV_FILE = env.str("ENV_FILE", default=".env")
+env.read_env(os.path.join(BASE_DIR, ENV_FILE))
+
+
+class Settings:
+    ADMIN_PREFIX = env.str("ADMIN_PREFIX", "admin")
+    ADMIN_USER_MODEL = env.str("ADMIN_USER_MODEL", "User")
+    ADMIN_USER_MODEL_USERNAME_FIELD = env.str("ADMIN_USER_MODEL_USERNAME_FIELD", "username")
+    ADMIN_SESSION_ID_KEY = env.str("ADMIN_SESSION_ID_KEY", "admin_session_id")
+    ADMIN_SITE_NAME = env.str("ADMIN_SITE_NAME", "API Administration")
+    ADMIN_SITE_SIGN_IN_LOGO = env.str(
+        "ADMIN_SITE_SIGN_IN_LOGO",
+        "/admin/static/images/sign-in-logo.png",
+    )
+    ADMIN_SITE_HEADER_LOGO = env.str(
+        "ADMIN_SITE_HEADER_LOGO",
+        "/admin/static/images/header-logo.svg",
+    )
+    ADMIN_SITE_FAVICON = env.str(
+        "ADMIN_SITE_FAVICON",
+        "/admin/static/images/favicon.png",
+    )
+    ADMIN_PRIMARY_COLOR = env.str("ADMIN_PRIMARY_COLOR", "#009485")
+
+
+settings = Settings()
