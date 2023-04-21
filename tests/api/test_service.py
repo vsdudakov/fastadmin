79d6180f094079626a83ad0fd817e508ed91f22b
Seva D <vsdudakov@gmail.com>
2023-04-22 03:53:48 +0400

    Fixed issue with str user_id

--- tests/api/test_service.py
+++ tests/api/test_service.py
@@ -1,7 +1,5 @@
 from uuid import UUID
 
-import pytest
-
 from fastadmin.api.service import convert_id
 
 
@@ -11,5 +9,4 @@ def test_convert_id():
     assert convert_id(1000) == 1000
     assert convert_id("123e4567-e89b-12d3-a456-426614174000") == UUID("123e4567-e89b-12d3-a456-426614174000")
     assert convert_id(UUID("123e4567-e89b-12d3-a456-426614174000")) == UUID("123e4567-e89b-12d3-a456-426614174000")
-    with pytest.raises(ValueError):
-        assert convert_id("invalid")
+    assert convert_id("invalid") is None
