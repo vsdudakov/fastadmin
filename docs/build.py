b823e292511bfc7d20ce7ea943f601be95e42d50
vsdudakov <vsdudakov@gmail.com>
2025-01-03 13:56:55 +0300

    Fix for is_pk

--- docs/build.py
+++ docs/build.py
@@ -40,6 +40,12 @@ def read_cls_docstring(cls):
 def get_versions():
     return [
         {
+            "version": "0.2.19",
+            "changes": [
+                "Fix for is_pk for tortoise orm.",
+            ],
+        },
+        {
             "version": "0.2.18",
             "changes": [
                 "Fixes for m2m fk's sqlalchemy postgres. Convert str to int for them.",
