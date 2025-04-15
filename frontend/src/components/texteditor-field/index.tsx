99230292561169c4dc96c8f7b616dce2bef97672
vsdudakov <vsdudakov@gmail.com>
2025-04-15 08:37:23 +0300

    Bump quill

--- frontend/src/components/texteditor-field/index.tsx
+++ frontend/src/components/texteditor-field/index.tsx
@@ -1,7 +1,7 @@
 import type React from "react";
-import ReactQuill from "react-quill";
+import ReactQuill from "react-quill-new";
 
-import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
 
 interface ITextEditorFieldProps {
   value?: any;
